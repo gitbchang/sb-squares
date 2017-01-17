f8697f4657bff9261f275aa80772e741d828f9b5
Brian Chang <Brian Chang>
2017-01-16 21:34:41 -0600

    signin

--- javascript/sbjava.js
+++ javascript/sbjava.js
@@ -17,30 +17,14 @@ $(document).ready(function(){
 var name;
 var profile;
 var email;
- function onSuccess(googleUser) {
-     console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-   }
-   function onFailure(error) {
-     console.log(error);
-   }
-   function renderButton() {
-     gapi.signin2.render('my-signin2', {
-       'scope': 'profile email',
-       'width': 240,
-       'height': 50,
-       'longtitle': true,
-       'theme': 'dark',
-       'onsuccess': onSuccess,
-       'onfailure': onFailure
-     });
-   }
-   function onSignIn(googleUser) {
+function onSignIn(googleUser) {
        profile = googleUser.getBasicProfile();
        name = profile.getName();
        //console.log('Image URL: ' + profile.getImageUrl());
        email = profile.getEmail();
        //initiiate();
        $('.g-signin2').hide();
+       console.log("You have Signed In");
 
    }
    function signOut() {
