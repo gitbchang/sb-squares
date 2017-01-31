6bcb448ab47ab68dd13714bd6fe2c126ec4fc0fc
gitbchang <brian.h.chang1@gmail.com>
2017-01-31 17:58:43 -0600

    changes

--- javascript/sbjava.js
+++ javascript/sbjava.js
@@ -122,7 +122,7 @@ $(document).ready(function() {
       });
     }
 
-/*
+
     // Google Sign On Variables
     var name;
     var profile;
@@ -130,24 +130,6 @@ $(document).ready(function() {
     // $(".g-signin2").trigger();
 
     function onSignIn(googleUser) {
-      firebase.auth().signInWithPopup(provider).then(function(result) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = result.credential.accessToken;
-          // The signed-in user info.
-          var user = result.user;
-
-          // ...
-      }).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          // ...
-      });
-
 
 
         profile = googleUser.getBasicProfile();
@@ -160,7 +142,7 @@ $(document).ready(function() {
 
   }
 
-  */
+
 
     function signOut() {
         var auth2 = gapi.auth2.getAuthInstance();
